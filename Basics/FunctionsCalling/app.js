1330687843937e9717d94d669d68203a9ece76b1
Daniil Meshcheriakov <sacretsid@gmail.com>
2023-07-21 18:10:51 +0400

    Functions

---
+++ Basics/FunctionsCalling/app.js
@@ -0,0 +1,12 @@
+"use strict";
+
+function colorMixer(color1, color2) {
+    return `${color1} + ${color2}`;
+}
+
+function colorizer(item, color1, color2) {
+    const color = colorMixer(color1, color2);
+    return `The ${item} is ${color}`;
+}
+
+console.log(colorizer("car", "red", "black"));
