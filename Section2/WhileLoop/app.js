e0d2f6e5209e123869ca5613df1707b391344f41
Daniil Meshcheriakov <sacretsid@gmail.com>
2023-07-25 13:59:40 +0400

    Rename

---
+++ Section2/WhileLoop/app.js
@@ -0,0 +1,9 @@
+let exercise = 1;
+while (exercise <= 3) {
+    let rep = 1;
+    while (rep <= 5) {
+        console.log(`Exercise ${exercise} Rep ${rep}`);
+        rep++;
+    }
+    exercise++;
+}
