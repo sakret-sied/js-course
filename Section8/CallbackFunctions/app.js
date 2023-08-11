1482415cb6d24037b0f91442cd6a03b3b2a7fd43
Daniil Meshcheriakov <sacretsid@gmail.com>
2023-08-11 16:52:46 +0400

    CallbackAndReturn

---
+++ Section8/CallbackFunctions/app.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const removeSpaces = function (text) {
+    return text.replace(/ /g, "");
+};
+const upperFirstWord = function (text) {
+    const [first, ...others] = text.split(" ");
+    return [first.toUpperCase(), ...others].join(" ");
+};
+const converter = function (str, fun) {
+    console.log(fun(str));
+};
+
+converter("Hello New World!", removeSpaces);
+converter("Hello New World!", upperFirstWord);
+
+const twice = (num) => console.log(num * 2);
+[1, 2, 3].forEach(twice);
