aab3f832f2cd7a6120247b31e14074e373236416
SakretSied <sacretsid@gmail.com>
2023-09-23 23:11:46 +0400

    RefactoringTransactions

--- Section09/SimplyBankApp/script.js
+++ Section09/SimplyBankApp/script.js
@@ -4,18 +4,17 @@
 
 const account1 = {
   userName: 'Cecil Ireland',
-  transactions: [500, 250, -300, 5000, -850, -110, -170, 1100],
   interest: 1.5,
   pin: 1111,
-  transactionsDates: [
-    '2020-10-02T14:43:31.074Z',
-    '2020-10-29T11:24:19.761Z',
-    '2020-11-15T10:45:23.907Z',
-    '2021-01-22T12:17:46.255Z',
-    '2021-02-12T15:14:06.486Z',
-    '2021-03-09T11:42:26.371Z',
-    '2023-09-21T15:21:20.814Z',
-    '2023-09-22T15:21:20.814Z',
+  transactions: [
+    { datetime: '2020-10-02T14:43:31.074Z', currency: 'USD', value: 500 },
+    { datetime: '2020-10-29T11:24:19.761Z', currency: 'USD', value: 250 },
+    { datetime: '2020-11-15T10:45:23.907Z', currency: 'USD', value: -300 },
+    { datetime: '2021-01-22T12:17:46.255Z', currency: 'USD', value: 5000 },
+    { datetime: '2021-02-12T15:14:06.486Z', currency: 'USD', value: -850 },
+    { datetime: '2021-03-09T11:42:26.371Z', currency: 'USD', value: -110 },
+    { datetime: '2023-09-21T15:21:20.814Z', currency: 'USD', value: -170 },
+    { datetime: '2023-09-22T15:21:20.814Z', currency: 'USD', value: 1100 },
   ],
   currency: 'USD',
   locale: 'en-US',
@@ -23,18 +22,17 @@ const account1 = {
 
 const account2 = {
   userName: 'Amani Salt',
-  transactions: [2000, 6400, -1350, -70, -210, -2000, 5500, -30],
   interest: 1.3,
   pin: 2222,
-  transactionsDates: [
-    '2020-10-02T14:43:31.074Z',
-    '2020-10-29T11:24:19.761Z',
-    '2020-11-15T10:45:23.907Z',
-    '2021-01-22T12:17:46.255Z',
-    '2021-02-12T15:14:06.486Z',
-    '2021-03-09T11:42:26.371Z',
-    '2021-05-21T07:43:59.331Z',
-    '2021-06-22T15:21:20.814Z',
+  transactions: [
+    { datetime: '2020-10-02T14:43:31.074Z', currency: 'UAH', value: 2000 },
+    { datetime: '2020-10-29T11:24:19.761Z', currency: 'UAH', value: 6400 },
+    { datetime: '2020-11-15T10:45:23.907Z', currency: 'UAH', value: -1350 },
+    { datetime: '2021-01-22T12:17:46.255Z', currency: 'UAH', value: -70 },
+    { datetime: '2021-02-12T15:14:06.486Z', currency: 'UAH', value: -210 },
+    { datetime: '2021-03-09T11:42:26.371Z', currency: 'UAH', value: -2000 },
+    { datetime: '2023-09-21T15:21:20.814Z', currency: 'UAH', value: 5500 },
+    { datetime: '2023-09-22T15:21:20.814Z', currency: 'UAH', value: -30 },
   ],
   currency: 'UAH',
   locale: 'uk-UA',
@@ -42,18 +40,17 @@ const account2 = {
 
 const account3 = {
   userName: 'Corey Martinez',
-  transactions: [900, -200, 280, 300, -200, 150, 1400, -400],
   interest: 0.8,
   pin: 3333,
-  transactionsDates: [
-    '2020-10-02T14:43:31.074Z',
-    '2020-10-29T11:24:19.761Z',
-    '2020-11-15T10:45:23.907Z',
-    '2021-01-22T12:17:46.255Z',
-    '2021-02-12T15:14:06.486Z',
-    '2021-03-09T11:42:26.371Z',
-    '2021-05-21T07:43:59.331Z',
-    '2021-06-22T15:21:20.814Z',
+  transactions: [
+    { datetime: '2020-10-02T14:43:31.074Z', currency: 'RUB', value: 900 },
+    { datetime: '2020-10-29T11:24:19.761Z', currency: 'RUB', value: -200 },
+    { datetime: '2020-11-15T10:45:23.907Z', currency: 'RUB', value: 280 },
+    { datetime: '2021-01-22T12:17:46.255Z', currency: 'RUB', value: 300 },
+    { datetime: '2021-02-12T15:14:06.486Z', currency: 'RUB', value: -200 },
+    { datetime: '2021-03-09T11:42:26.371Z', currency: 'RUB', value: 150 },
+    { datetime: '2023-09-21T15:21:20.814Z', currency: 'RUB', value: 1400 },
+    { datetime: '2023-09-22T15:21:20.814Z', currency: 'RUB', value: -400 },
   ],
   currency: 'RUB',
   locale: 'ru-RU',
@@ -61,15 +58,14 @@ const account3 = {
 
 const account4 = {
   userName: 'Kamile Searle',
-  transactions: [530, 1300, 500, 40, 190],
   interest: 1,
   pin: 4444,
-  transactionsDates: [
-    '2020-10-02T14:43:31.074Z',
-    '2020-10-29T11:24:19.761Z',
-    '2020-11-15T10:45:23.907Z',
-    '2021-01-22T12:17:46.255Z',
-    '2021-02-12T15:14:06.486Z',
+  transactions: [
+    { datetime: '2020-10-02T14:43:31.074Z', currency: 'EUR', value: 530 },
+    { datetime: '2020-10-29T11:24:19.761Z', currency: 'EUR', value: 1300 },
+    { datetime: '2020-11-15T10:45:23.907Z', currency: 'EUR', value: 500 },
+    { datetime: '2021-01-22T12:17:46.255Z', currency: 'EUR', value: 40 },
+    { datetime: '2021-02-12T15:14:06.486Z', currency: 'EUR', value: 190 },
   ],
   currency: 'EUR',
   locale: 'fr-CA',
@@ -80,12 +76,12 @@ const account5 = {
   transactions: [630, 800, 300, 50, 120],
   interest: 1.1,
   pin: 5555,
-  transactionsDates: [
-    '2020-10-02T14:43:31.074Z',
-    '2020-10-29T11:24:19.761Z',
-    '2020-11-15T10:45:23.907Z',
-    '2021-01-22T12:17:46.255Z',
-    '2021-02-12T15:14:06.486Z',
+  transactions: [
+    { datetime: '2020-10-02T14:43:31.074Z', currency: 'USD', value: 630 },
+    { datetime: '2020-10-29T11:24:19.761Z', currency: 'USD', value: 800 },
+    { datetime: '2020-11-15T10:45:23.907Z', currency: 'USD', value: 300 },
+    { datetime: '2021-01-22T12:17:46.255Z', currency: 'USD', value: 50 },
+    { datetime: '2021-02-12T15:14:06.486Z', currency: 'USD', value: 120 },
   ],
   currency: 'USD',
   locale: 'en-US',
@@ -139,7 +135,7 @@ const getDate = function (date, locale = navigator.language, isText = false) {
   }
 };
 
-const getCurrency = (locale, currency, value) =>
+const getCurrency = (value, currency, locale) =>
   new Intl.NumberFormat(locale, {
     style: 'currency',
     currency: currency,
@@ -196,7 +192,7 @@ const transfer = function (e) {
 
   const transferAmount = +inputTransferAmount.value;
   const recipientNickname = inputTransferTo.value;
-  const recipientAcoount = accounts.find(
+  const recipientAccount = accounts.find(
     (account) => account.nickname === recipientNickname
   );
 
@@ -206,14 +202,19 @@ const transfer = function (e) {
   if (
     transferAmount > 0 &&
     currentAccount.balance >= transferAmount &&
-    recipientAcoount &&
-    currentAccount.nickname !== recipientAcoount.nickname
+    recipientAccount &&
+    currentAccount.nickname !== recipientAccount.nickname
   ) {
-    currentAccount.transactions.push(-transferAmount);
-    currentAccount.transactionsDates.push(new Date().toISOString());
-
-    recipientAcoount.transactions.push(transferAmount);
-    recipientAcoount.transactionsDates.push(new Date().toISOString());
+    currentAccount.transactions.push({
+      datetime: new Date().toISOString(),
+      currency: currentAccount.currency,
+      value: -transferAmount,
+    });
+    recipientAccount.transactions.push({
+      datetime: new Date().toISOString(),
+      currency: currentAccount.currency,
+      value: transferAmount,
+    });
 
     updateUI();
   }
@@ -248,11 +249,14 @@ const loan = function (e) {
   if (
     loanAmount > 0 &&
     currentAccount.transactions.some(
-      (trans) => trans >= (loanAmount * 10) / 100
+      (trans) => trans.value >= (loanAmount * 10) / 100
     )
   ) {
-    currentAccount.transactions.push(loanAmount);
-    currentAccount.transactionsDates.push(new Date().toISOString());
+    currentAccount.transactions.push({
+      datetime: new Date().toISOString(),
+      currency: currentAccount.currency,
+      value: loanAmount,
+    });
 
     updateUI();
   }
@@ -273,19 +277,16 @@ const updateUI = function () {
   displayTotal(currentAccount);
 };
 
-const displayTransactions = function (
-  { transactions, transactionsDates, currency, locale },
-  sort = false
-) {
+const displayTransactions = function ({ transactions, locale }, sort = false) {
   containerTransactions.innerHTML = '';
 
   const transactionsSort = sort
-    ? transactions.slice().sort((x, y) => x - y)
+    ? transactions.slice().sort((x, y) => x.value - y.value)
     : transactions;
 
   transactionsSort.forEach(function (trans, index) {
-    const transType = trans > 0 ? 'deposit' : 'withdrawal';
-    const transDate = getDate(new Date(transactionsDates[index]), locale, true);
+    const transType = trans.value > 0 ? 'deposit' : 'withdrawal';
+    const transDate = getDate(new Date(trans.datetime), locale, true);
 
     const transactionRow = `
     <div class="transactions__row">
@@ -294,9 +295,9 @@ const displayTransactions = function (
       </div>
       <div class="transactions__date">${transDate}</div>
       <div class="transactions__value">${getCurrency(
-        locale,
-        currency,
-        trans
+        trans.value,
+        trans.currency,
+        locale
       )}</div>
     </div>
     `;
@@ -305,33 +306,37 @@ const displayTransactions = function (
 };
 
 const displayBalance = function (account) {
-  account.balance = account.transactions.reduce((prev, curr) => prev + curr, 0);
+  account.balance = account.transactions
+    .map((trans) => trans.value)
+    .reduce((prev, curr) => prev + curr, 0);
   labelBalance.textContent = getCurrency(
-    account.locale,
+    account.balance,
     account.currency,
-    account.balance
+    account.locale
   );
 };
 
 const displayTotal = function ({ transactions, interest, currency, locale }) {
   const depositesTotal = transactions
-    .filter((trans) => trans > 0)
+    .filter((trans) => trans.value > 0)
+    .map((trans) => trans.value)
     .reduce((acc, trans) => acc + trans, 0);
-  labelSumIn.textContent = getCurrency(locale, currency, depositesTotal);
+  labelSumIn.textContent = getCurrency(depositesTotal, currency, locale);
 
   const withdrawalsTotal = transactions
-    .filter((trans) => trans < 0)
+    .filter((trans) => trans.value < 0)
+    .map((trans) => trans.value)
     .reduce((acc, trans) => acc + trans, 0);
-  labelSumOut.textContent = getCurrency(locale, currency, withdrawalsTotal);
+  labelSumOut.textContent = getCurrency(withdrawalsTotal, currency, locale);
 
   const interestTotal = transactions
-    .filter((trans) => trans > 0)
-    .map((depos) => (depos * interest) / 100)
-    .filter((interes, _, arr) => {
-      return interes >= 5;
+    .filter((trans) => trans.value > 0)
+    .map((trans) => (trans.value * interest) / 100)
+    .filter((interest) => {
+      return interest >= 5;
     })
     .reduce((acc, interest) => acc + interest, 0);
-  labelSumInterest.textContent = getCurrency(locale, currency, interestTotal);
+  labelSumInterest.textContent = getCurrency(interestTotal, currency, locale);
 };
 
 // Execute
