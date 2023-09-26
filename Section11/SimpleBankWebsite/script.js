c6671b65cccbb5255e1f73991a468d5656dfa97e
SakretSied <sacretsid@gmail.com>
2023-09-26 20:23:58 +0400

    DOMTraversing

--- Section11/SimpleBankWebsite/script.js
+++ Section11/SimpleBankWebsite/script.js
@@ -34,7 +34,30 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-///////////////////////////////////
+// Scrolling
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+// Smooth navigation
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  if (!e.target.classList.contains('nav__link')) {
+    return;
+  }
+  const href = e.target.getAttribute('href');
+  document.querySelector(href).scrollIntoView({ behavior: 'smooth' });
+  console.log(href);
+});
+
+////////////////////////////////////////////
+///////////////// Examples /////////////////
+////////////////////////////////////////////
 
 // console.log(document.documentElement);
 // console.log(document.head);
@@ -76,7 +99,7 @@ document.addEventListener('keydown', function (e) {
 //   // message.parentElement.removeChild(message);
 // });
 
-// Style
+// // Style
 
 // message.style.backgroundColor = '#076785';
 // message.style.width = '120%';
@@ -89,7 +112,7 @@ document.addEventListener('keydown', function (e) {
 
 // document.documentElement.style.setProperty('--color-first', 'yellow');
 
-// Attributes
+// // Attributes
 
 // const logo = document.querySelector('.nav__logo');
 // console.log(logo.alt);
@@ -115,50 +138,103 @@ document.addEventListener('keydown', function (e) {
 // logo.classList.toggle('a');
 // logo.classList.contains('c');
 
-// DO NOT USE
+// // DO NOT USE
 
 // logo.className = 'a';
 
-// Scrolling
+// // Scrolling
 
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
+// const btnScrollTo = document.querySelector('.btn--scroll-to');
+// const section1 = document.querySelector('#section--1');
 
-btnScrollTo.addEventListener('click', function (e) {
-  const section1Coords = section1.getBoundingClientRect();
-  console.log(section1Coords);
-  console.log(e.target.getBoundingClientRect());
-  console.log('Текущее прокручивание: x, y', window.scrollX, window.scrollY);
-  console.log(
-    'Ширина и высота viewport',
-    document.documentElement.clientWidth,
-    document.documentElement.clientHeight,
-  );
+// btnScrollTo.addEventListener('click', function (e) {
+//   const section1Coords = section1.getBoundingClientRect();
+//   console.log(section1Coords);
+//   console.log(e.target.getBoundingClientRect());
+//   console.log('Текущее прокручивание: x, y', window.scrollX, window.scrollY);
+//   console.log(
+//     'Ширина и высота viewport',
+//     document.documentElement.clientWidth,
+//     document.documentElement.clientHeight,
+//   );
+//   section1.scrollIntoView({ behavior: 'smooth' });
 
-  // window.scrollTo(
-  //   section1Coords.left + window.scrollX,
-  //   section1Coords.top + window.scrollY
-  // );
+//   // window.scrollTo(
+//   //   section1Coords.left + window.scrollX,
+//   //   section1Coords.top + window.scrollY
+//   // );
 
-  // window.scrollTo({
-  //   left: section1Coords.left + window.scrollX,
-  //   top: section1Coords.top + window.scrollY,
-  //   behavior: 'smooth',
-  // });
+//   // window.scrollTo({
+//   //   left: section1Coords.left + window.scrollX,
+//   //   top: section1Coords.top + window.scrollY,
+//   //   behavior: 'smooth',
+//   // });
+// });
 
-  section1.scrollIntoView({ behavior: 'smooth' });
-});
+// // Events
 
-// Events
-
-const h1 = document.querySelector('h1');
-const mouseEnter = function (e) {
-  alert('addEventListener: You are now at the h1 element');
-};
-h1.addEventListener('mouseenter', mouseEnter);
-setTimeout(function () {
-  h1.removeEventListener('mouseenter', mouseEnter);
-}, 3000);
+// const h1 = document.querySelector('h1');
+// const mouseEnter = function (e) {
+//   alert('addEventListener: You are now at the h1 element');
+// };
+// h1.addEventListener('mouseenter', mouseEnter);
+// setTimeout(function () {
+//   h1.removeEventListener('mouseenter', mouseEnter);
+// }, 3000);
 // h1.onclick = function (e) {
 //   alert('onclick: You click');
 // };
+
+// // Event Propagation
+
+// function getRand(min, max) {
+//   min = Math.ceil(min);
+//   max = Math.floor(max);
+//   return Math.floor(Math.random() * (max - min + 1) + min);
+// }
+// const getRandomColor = () =>
+//   `rgb(${getRand(0, 255)}, ${getRand(0, 255)}, ${getRand(0, 255)})`;
+// const backgroundRandom = function (e) {
+//   e.preventDefault();
+//   this.style.backgroundColor = getRandomColor();
+//   // e.stopPropagation();
+// };
+
+// document
+//   .querySelectorAll('.nav__link')
+//   .forEach((element) => element.addEventListener('click', backgroundRandom));
+// document
+//   .querySelector('.nav__links')
+//   .addEventListener('click', backgroundRandom);
+// document
+//   .querySelector('nav')
+//   .addEventListener('click', backgroundRandom /*, true*/);
+// document.querySelector('body').addEventListener('click', backgroundRandom);
+
+// DOM traversing
+
+// const h1 = document.querySelector('h1');
+
+// // Childs
+
+// console.log(h1.querySelectorAll('.highlight'));
+// console.log(h1.childNodes);
+// console.log(h1.children);
+// console.log(h1.firstElementChild);
+// h1.firstElementChild.style.color = 'yellow';
+// console.log(h1.lastElementChild);
+
+// // Parents
+
+// console.log(h1.parentNode);
+// console.log(h1.parentElement);
+// const h2 = document.querySelector('h2');
+// h2.closest('.section').style.backgroundColor = 'blue';
+// h2.closest('h2').style.backgroundColor = 'green';
+
+// // Siblings
+
+// console.log(h2.previousElementSibling);
+// console.log(h2.nextElementSibling);
+
+// console.log(h1.parentElement.children);
