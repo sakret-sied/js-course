3c124e6e2091c0b457a630ee508ee863a90a87d3
SakretSied <sacretsid@gmail.com>
2023-11-11 21:24:41 +0400

    remove duplicate

--- 14.Async/app.async.js
+++ 14.Async/app.async.js
@@ -6,7 +6,6 @@ import PromiseFunctions from './modules/promise.functions.js';
 import PromiseTask1 from './modules/promise.task1.js';
 import PromiseTask2 from './modules/promise.task2.js';
 import PromiseTask3 from './modules/promise.task3.js';
-import Promisify from './modules/promisify.js';
 import LittleCore from '/node_modules/js-little-core/little.core.js';
 
 class AppAsync extends LittleCore {
@@ -21,7 +20,6 @@ class AppAsync extends LittleCore {
       PromiseTask1,
       PromiseTask2,
       PromiseTask3,
-      Promisify,
     ]);
   }
 }
--- 14.Async/modules/async.await.js
+++ 14.Async/modules/async.await.js
@@ -3,13 +3,6 @@ import ModuleWithCountry from '../classes/module.with.country.js';
 
 export default class AsyncAwait extends ModuleWithCountry {
   execute() {
-    const data = [
-      [35.756, 139.256],
-      [48.857, 2.358],
-      [40.708, -74.051],
-    ];
-    // data.forEach(([lat, lon]) => new Country().getCountryByGPS(lat, lon));
-
     const country = new Country();
     country.btn.addEventListener('click', function () {
       country.getUserCountry();
